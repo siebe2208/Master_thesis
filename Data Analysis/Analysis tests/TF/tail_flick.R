408875a48c7667758a91f42bf1e0e0900d24b463
siebe2208 <siebeeveraerts1@gmail.com>
2026-04-15 16:25:15 +0200

    updates

---
+++ Data Analysis/Analysis tests/TF/tail_flick.R
@@ -0,0 +1,55 @@
+########################################
+
+# Important !!!!!
+
+# environment 
+# 0 = PE
+# 1 = EE
+
+# treatment
+# 0 = SAL
+# 1 = CVAD
+
+########################################
+
+#####################
+## Load in packages##
+#####################
+pacman::p_load(tidyverse, ggplot2, here, emmeans, logspline, cmdstanr, posterior, rstanarm, HDInterval)
+path = here("Data Analysis", "Analysis tests", "TF", "fits")
+source(here("Data Analysis", "unclean_data.R"))
+source(here("Data Analysis", "Utility.R"))
+
+########################################
+## Load in data and mutate conditions ##
+########################################
+data = data.frame(animal = mice_ID, treatment = treatment, env = environment, T1 = Tail_flick1, 
+                  T2 = Tail_flick2, T3 = Tail_flick3)
+
+data = data %>% rowwise() %>% 
+  mutate(treatment = factor(ifelse(treatment == "Sal", 0, 1))) %>% 
+  mutate(env = factor(ifelse(env == "PE", 0,1))) %>% 
+  mutate(mean = (T1 +T2 +T3)/3)
+
+
+sum(as.numeric(data$treatment)-1) # Check how many animals CVAD (should be 12)
+sum(as.numeric(data$env)-1)  # Check how many animals CVAD (should be 18)
+
+data = data %>% filter(animal != "F13.2_B") # exclude the mouse that died 
+
+######################
+## Latency     freq ##
+######################
+ANOVA = two_way_aov(data, "treatment", "env", "mean")
+
+con = emmeans(ANOVA[[3]], pairwise ~ treatment*env, adjust = "tukey")
+
+############################
+## Two‑way ANOVA Bayesian ##
+############################
+mod = cmdstan_model(here("Data Analysis", "Analysis tests", "TF", "ANOVA_TF.stan"))
+path_TF = here(path, "fit_TF.rds")
+
+fit = two_way_bayes(mod, data, "mean", path_TF, 7)
+
+
